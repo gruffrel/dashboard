a1edda856889d6b7bca9c3b14b238174a46597f7
Steve Poole <spoole167@googlemail.com>
2022-12-08 21:39:14 -0500

    try 2 oauth redirect

--- js/auth.js
+++ js/auth.js
@@ -5,7 +5,8 @@ if(sessionStorage.getItem("dashboard-code")==null) {
   fudge=sessionStorage.getItem("dashboard-fudge");
   logButton=document.getElementById('signin');
 
-  signin.innerHTML="<a class='button' href='https://app.asana.com/-/oauth_authorize?response_type=code&client_id=1203516656368369&redirect_uri=https%3A%2F%2Fgruffwizard.dev%2Fdashboard%2Fcallback.html&state="+fudge+"'>Sign in</a>";
+  var href="https://app.asana.com/-/oauth_authorize?response_type=code&client_id=1203516656368369&redirect_uri=https%3A%2F%2Fgruffwizard.dev%2Fdashboard%2Fcallback.html&state="+fudge;
+  signin.innerHTML="<a class='button' href='"+href+"'>Sign in</a>";
 
 
 }
