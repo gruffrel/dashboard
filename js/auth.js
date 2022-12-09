7e284b03f1f78b1da8e685be088972ee7a81a278
Steve Poole <spoole167@googlemail.com>
2022-12-08 21:34:37 -0500

    fix callback uri

--- js/auth.js
+++ js/auth.js
@@ -5,7 +5,7 @@ if(sessionStorage.getItem("dashboard-code")==null) {
   fudge=sessionStorage.getItem("dashboard-fudge");
   logButton=document.getElementById('signin');
 
-  signin.innerHTML="<a class='button' href='https://app.asana.com/-/oauth_authorize?response_type=code&client_id=1203516656368369&redirect_uri=https%3A%2F%2Fgruffwizard.dev%2Fdashboard%2F&state="+fudge+"'>Sign in</a>";
+  signin.innerHTML="<a class='button' href='https://app.asana.com/-/oauth_authorize?response_type=code&client_id=1203516656368369&redirect_uri=https%3A%2F%2Fgruffwizard.dev%2Fdashboard%2Fcallback.html&state="+fudge+"'>Sign in</a>";
 
 
 }
